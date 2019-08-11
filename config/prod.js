e5e1967b080114a1325c3506832afdc44a1412ae
Prakash Kumar <psingh5293@gmail.com>
2019-08-11 17:09:09 +0530

    config changes

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
-  secret: "lasjdalskhdjahjhdsa",
-  mongoURI:
-    "mongodb+srv://psingh5293:xRRpunzLazsUuDM6@tridentpool-wo2pk.mongodb.net/test?retryWrites=true&w=majority"
+  secret: process.env.SECRET,
+  mongoURI: process.env.MONGO_URI
 };
