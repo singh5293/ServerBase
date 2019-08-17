fe9cf39ccf5b594474be26975f05471f48138110
Prakash Kumar <psingh5293@gmail.com>
2019-08-17 21:21:41 +0530

    prod key changes

--- config/prod.js
+++ config/prod.js
@@ -1,18 +1,5 @@
 module.exports = {
-  secret: "lasjdalskhdjahjhdsa",
-  sendGridKey:
-    "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",
-  mongoURI:
-    "mongodb+srv://psingh5293:xRRpunzLazsUuDM6@tridentpool-wo2pk.mongodb.net/test?retryWrites=true&w=majority"
+  secret: process.env.SECRET,
+  sendGridKey: process.env.SENDGRID_KEY,
+  mongoURI: process.env.MONGO_URI
 };
-// module.exports = {
-//   secret: "lasjdalskhdjahjhdsa",
-//   mongoURI:
-//     "mongodb+srv://psingh5293:xRRpunzLazsUuDM6@tridentpool-wo2pk.mongodb.net/test?retryWrites=true&w=majority"
-// };
-
-//
-//mongodb+srv://psingh5293:xRRpunzLazsUuDM6@tridentpool-wo2pk.mongodb.net/test?retryWrites=true&w=majority
-
-//mongodb+srv://psingh5293p:Dbf5GkCNBaiwID4W@cluster0-oafnl.mongodb.net/test?retryWrites=true&w=majority
-// fdmsHjd8gJM4hgTJ
